58487153a65009c2adc979f2a0f99a2aa05fac55
jstrickler <jstrickler@gmail.com>
2023-12-06 09:58:23 -0500

    added slides

--- functions.py
+++ functions.py
@@ -1,4 +1,11 @@
-def say_hello():
-    print("Hello, Python world")
 
-say_hello()
+def sqrt(arg):
+    if isinstance(arg, (int, float)):
+        print(arg ** .5)
+    else:
+        raise TypeError('Argument must be a number')
+
+sqrt('abc')
+sqrt(123)
+sqrt([1,2,3])
+
